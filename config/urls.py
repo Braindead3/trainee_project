97b02f9740abb1974536dc0f86afc86d6f9a8584
vlad <malosay21@gmail.com>
2022-07-24 10:11:24 -0400

    add urls to work with all dealer models

--- config/urls.py
+++ config/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('src.car_showroom.urls'), name='car-showroom-models'),
     path('api/', include('src.customers.urls'), name='customer-models'),
-    # path('api/', include('src.dealer.urls'), name='dealer-viewset'),
+    path('api/', include('src.dealer.urls'), name='dealer-models'),
 ]
