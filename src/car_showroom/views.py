54b7b5efcfda1e394e504c459e34404a4583e71a
vlad <malosay21@gmail.com>
2022-07-25 06:23:10 -0400

    add jwt auth and some permissions to car showroom endpoints

--- src/car_showroom/views.py
+++ src/car_showroom/views.py
@@ -1,18 +1,22 @@
 from .models import CarShowroom, UniqueCustomer, ShowroomCustomerSale
 from core.base_view_sets import BaseViewSet
 from .serializers import CarShowroomSerializer, UniqueCustomerSerializer, ShowroomCustomerSaleSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class CarShowroomViewSet(BaseViewSet):
     serializer_class = CarShowroomSerializer
     queryset = CarShowroom.objects.all()
+    permission_classes = [IsAuthenticated, ]
 
 
 class UniqueCustomerViewSet(BaseViewSet):
     serializer_class = UniqueCustomerSerializer
     queryset = UniqueCustomer.objects.all()
+    permission_classes = [IsAuthenticated, ]
 
 
 class ShowroomCustomerSaleViewSet(BaseViewSet):
     serializer_class = ShowroomCustomerSaleSerializer
     queryset = ShowroomCustomerSale.objects.all()
+    permission_classes = [IsAuthenticated, ]
